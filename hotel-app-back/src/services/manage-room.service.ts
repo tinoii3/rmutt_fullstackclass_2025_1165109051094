afd4151e668d16b74faef854109e2260cc2fd15a
Tyrant09 <bosskung0880@gmail.com>
2026-03-09 18:37:43 +0700

    feat(manage-room.service): add getAllRoomTypes, createRoomType, createRoom, updateRoom, deleteRoom

--- hotel-app-back/src/services/manage-room.service.ts
+++ hotel-app-back/src/services/manage-room.service.ts
@@ -3,3 +3,23 @@ import * as manageRoomRepo from "../repository/manage-room.repository.js"
 export const getAllRooms = async () => {
     return manageRoomRepo.roomFindAll();
 }
+
+export const getAllRoomTypes = async () => {
+    return manageRoomRepo.roomTypeFindAll();
+}
+
+export const createRoom = async (data: any) => {
+    return manageRoomRepo.roomCreate(data);
+}
+
+export const createRoomType = async (data: any) => {
+    return manageRoomRepo.roomTypeCreate(data);
+}
+
+export const updateRoom = async (id: string, data: any) => {
+    return manageRoomRepo.roomUpdate(id, data);
+}
+
+export const deleteRoom = async (id: string) => {
+    return manageRoomRepo.roomDelete(id);
+}
